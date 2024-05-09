09762a65ffa299a0a7393b81fde0e7625a043375
JiHyun Lee <supernova63@naver.com>
2024-05-09 20:20:56 +0900

    refactor : UserIdol domain 내용 추가

--- beespring/src/main/java/com/beeSpring/beespring/domain/user/UserIdol.java
+++ beespring/src/main/java/com/beeSpring/beespring/domain/user/UserIdol.java
@@ -1,4 +1,15 @@
 package com.beeSpring.beespring.domain.user;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="User_Idol")
 public class UserIdol {
+    @Id
+    @Column(name="user_idol_id")
+    private int userIdolId;
+    @Column(name="serial_number")
+    private String serialNumber;
+    @Column(name="idol_id")
+    private int idolId;
 }
