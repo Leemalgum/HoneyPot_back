cdfb9d7749d2c9686719cf7300333fa5c985f44a
Leemalgum <withxia1215@naver.com>
2024-05-12 21:26:12 +0900

    feat: product dto 추가

--- beespring/src/main/java/com/beeSpring/beespring/dto/bid/ProductDTO.java
+++ beespring/src/main/java/com/beeSpring/beespring/dto/bid/ProductDTO.java
@@ -1,4 +1,34 @@
 package com.beeSpring.beespring.dto.bid;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
 public class ProductDTO {
+    private String productId;
+    private int idolId;
+    private int ptypeId;
+    private String serialNumber;
+    private String productName;
+    private String image1;
+    private String image2;
+    private String image3;
+    private String image4;
+    private String image5;
+    private String productInfo;
+    private int price;
+    private int priceUnit;
+    private int buyNow;
+    private LocalDateTime timeLimit;
+    private long view;
+    private int startPrice;
+    private LocalDateTime registrationDate;
+    private int bidCnt;
+    private String requestId;
+    private LocalDateTime requestTime;
+    private String storageStatus;
 }
+
