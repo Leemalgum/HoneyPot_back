70c15a973685f955eee6eb661d1f4f0cbcd6427c
Leemalgum <withxia1215@naver.com>
2024-05-12 21:25:59 +0900

    feat: product domain 추가

--- beespring/src/main/java/com/beeSpring/beespring/domain/bid/Product.java
+++ beespring/src/main/java/com/beeSpring/beespring/domain/bid/Product.java
@@ -1,4 +1,39 @@
 package com.beeSpring.beespring.domain.bid;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Product")
+@Getter
+@NoArgsConstructor
 public class Product {
+    @Id
+    private String productId;
+    private int idolId;
+    private int ptypeId;
+    private String serialNumber;
+    private String productName;
+    private String image1;
+    private String image2;
+    private String image3;
+    private String image4;
+    private String image5;
+    private String productInfo;
+    private int price;
+    private int priceUnit;
+    private int buyNow;
+    private LocalDateTime timeLimit;
+    private long view;
+    private int startPrice;
+    private LocalDateTime registrationDate;
+    private int bidCnt;
+    private String requestId;
+    private LocalDateTime requestTime;
+    private String storageStatus;
 }
