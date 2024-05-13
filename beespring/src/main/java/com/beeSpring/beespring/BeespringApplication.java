5dcdcede3cb5c36314457cccb1d46ea1de338c6f
TaeJinKim0930 <tjmax0930@gmail.com>
2024-05-13 14:48:35 +0900

    FIX: pom.xml dependencies 수정
    기존에 있던 redis 및 openai 등 아직 쓰지 않는 dependency 들 전부 주석 처리 완료

--- beespring/src/main/java/com/beeSpring/beespring/BeespringApplication.java
+++ beespring/src/main/java/com/beeSpring/beespring/BeespringApplication.java
@@ -1,11 +1,12 @@
 package com.beeSpring.beespring;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BeespringApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BeespringApplication.class, args);
 	}
