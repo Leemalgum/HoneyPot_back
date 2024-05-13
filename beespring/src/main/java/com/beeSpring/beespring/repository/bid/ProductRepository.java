5d7095c41fffb73f755e1ffdca5426067b0b0961
Leemalgum <withxia1215@naver.com>
2024-05-13 20:39:54 +0900

    feat: productrepository추가

--- beespring/src/main/java/com/beeSpring/beespring/repository/bid/ProductRepository.java
+++ beespring/src/main/java/com/beeSpring/beespring/repository/bid/ProductRepository.java
@@ -2,8 +2,14 @@ package com.beeSpring.beespring.repository.bid;
 
 import com.beeSpring.beespring.domain.bid.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProductRepository  extends JpaRepository<Product, String> {
+
+    @Query("select p, i.idolName from Product p join Idol i on p.idolId = i.idolId")
+    List<Object[]> findAllWithIdolName();
 }
