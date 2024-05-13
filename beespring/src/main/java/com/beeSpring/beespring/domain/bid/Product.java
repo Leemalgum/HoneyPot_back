2b6a68451276b728c211be6dff8e347530c248d3
Leemalgum <withxia1215@naver.com>
2024-05-13 20:40:18 +0900

    feat: product, ldol entitiy 추가

--- beespring/src/main/java/com/beeSpring/beespring/domain/bid/Product.java
+++ beespring/src/main/java/com/beeSpring/beespring/domain/bid/Product.java
@@ -5,8 +5,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-
 
 import java.time.LocalDateTime;
 
--- beespring/src/main/java/com/beeSpring/beespring/domain/category/Idol.java
+++ beespring/src/main/java/com/beeSpring/beespring/domain/category/Idol.java
@@ -1,4 +1,18 @@
 package com.beeSpring.beespring.domain.category;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.TypeAlias;
+
+@Entity
+@Table(name = "Idol", catalog = "honeypot2")
+@Getter
+@NoArgsConstructor
 public class Idol {
+    @Id
+    private int idolId;
+    private String idolName;
 }
