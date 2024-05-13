49fb929a3a4499eb589edab116e640b8441b2406
Leemalgum <withxia1215@naver.com>
2024-05-13 20:39:29 +0900

    feat: ProductWithIdolNameDTO idolName 컬럼 갖는 DTO추가

---
+++ beespring/src/main/java/com/beeSpring/beespring/dto/bid/ProductWithIdolNameDTO.java
@@ -0,0 +1,35 @@
+package com.beeSpring.beespring.dto.bid;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ProductWithIdolNameDTO {
+    private String productId;
+    private String idolName;
+    private int ptypeId;
+    private String serialNumber;
+    private String productName;
+    private String image1;
+    private String image2;
+    private String image3;
+    private String image4;
+    private String image5;
+    private String productInfo;
+    private int price;
+    private Integer priceUnit;
+    private int buyNow;
+    private LocalDateTime timeLimit;
+    private long view;
+    private int startPrice;
+    private LocalDateTime registrationDate;
+    private int bidCnt;
+    private String requestId;
+    private LocalDateTime requestTime;
+    private String storageStatus;
+}
