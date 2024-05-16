a89bb2046044b09ba92f47eadc06f53e5ab12b53
JiHyun Lee <supernova63@naver.com>
2024-05-16 09:41:54 +0900

    feat : UserRepository 추가

--- beespring/src/main/java/com/beeSpring/beespring/repository/user/UserRepository.java
+++ beespring/src/main/java/com/beeSpring/beespring/repository/user/UserRepository.java
@@ -2,10 +2,16 @@ package com.beeSpring.beespring.repository.user;
 
 import com.beeSpring.beespring.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUserId(String userId);
-
+public interface UserRepository extends JpaRepository<User, String> {
+    Boolean existsByUserId(String userId);
+    /**
+     * sql injection 을 예방하기 위해 userId를 parameter 로 binding 함
+     * */
+    @Query("SELECT u FROM User u WHERE u.user_id = :userId")
+    User findByUserId(@Param("userId") String userId);
 }
