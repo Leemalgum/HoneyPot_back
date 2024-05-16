b4b5470bd6aa0971dede54cce37f69d1c96e5378
Leemalgum <withxia1215@naver.com>
2024-05-16 12:04:21 +0900

    feat: Idol domain 추가

--- beespring/src/main/java/com/beeSpring/beespring/domain/category/Idol.java
+++ beespring/src/main/java/com/beeSpring/beespring/domain/category/Idol.java
@@ -1,18 +1,21 @@
 package com.beeSpring.beespring.domain.category;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.TypeAlias;
 
 @Entity
-@Table(name = "Idol", catalog = "honeypot2")
+@Table(name = "Idol")
 @Getter
 @NoArgsConstructor
 public class Idol {
     @Id
+    @Column(name = "idol_id")
     private int idolId;
+
+    @Column(name = "idol_name")
     private String idolName;
 }
