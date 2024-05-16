95bfc070b0a33603d11d927c2be5e383dd912a21
Leemalgum <withxia1215@naver.com>
2024-05-16 12:07:37 +0900

    feat: ProductRepository findBySerialNumber 메서드 추가

--- beespring/src/main/java/com/beeSpring/beespring/repository/bid/ProductRepository.java
+++ beespring/src/main/java/com/beeSpring/beespring/repository/bid/ProductRepository.java
@@ -1,8 +1,10 @@
 package com.beeSpring.beespring.repository.bid;
 
 import com.beeSpring.beespring.domain.bid.Product;
+import com.beeSpring.beespring.dto.mypage.ProductWithSerialNumberDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,4 +14,7 @@ public interface ProductRepository  extends JpaRepository<Product, String> {
 
     @Query("select p, i.idolName from Product p join Idol i on p.idolId = i.idolId")
     List<Object[]> findAllWithIdolName();
+    
+    @Query("SELECT br.productId, br.paymentStatus, sh.deliveryStatus, p.serialNumber, p.productName, p.image1, p.priceUnit, p.startPrice, p.bidCnt FROM BidResult br JOIN Shipping sh ON br.bidResultId = sh.bidResult.bidResultId JOIN Product p ON br.productId = p.productId")
+    List<Object[]> findBySerialNumber(String serialNumber);
 }
