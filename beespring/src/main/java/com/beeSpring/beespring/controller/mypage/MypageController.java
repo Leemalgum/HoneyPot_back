aae12b247ed7875bebe0210ff8b631f7efd768de
Leemalgum <withxia1215@naver.com>
2024-05-16 12:03:16 +0900

    feat: MypageController 판매목록 api 추가

--- beespring/src/main/java/com/beeSpring/beespring/controller/mypage/MypageController.java
+++ beespring/src/main/java/com/beeSpring/beespring/controller/mypage/MypageController.java
@@ -1,7 +1,32 @@
 package com.beeSpring.beespring.controller.mypage;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.beeSpring.beespring.dto.bid.ProductDTO;
+import com.beeSpring.beespring.dto.mypage.ProductWithSerialNumberDTO;
+import com.beeSpring.beespring.service.mypage.MypageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
 public class MypageController {
+    private final MypageService mypageService;
+
+    @GetMapping(path = "/products/{serialNumber}")
+    public ResponseEntity<List<ProductWithSerialNumberDTO>> getProductList(@PathVariable("serialNumber") String serialNumber) {
+        // serialNumber를 이용하여 ProductService를 통해 해당 serialNumber에 해당하는 상품 목록을 가져옵니다.
+        List<ProductWithSerialNumberDTO> productList = mypageService.getProductListBySerialNumber(serialNumber);
+        if (!productList.isEmpty()) {
+            // 상품 목록이 존재할 경우 200 OK 응답과 함께 데이터를 반환합니다.
+            return new ResponseEntity<>(productList, HttpStatus.OK);
+        } else {
+            // 상품 목록이 존재하지 않을 경우 404 Not Found 응답을 반환합니다.
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 }
