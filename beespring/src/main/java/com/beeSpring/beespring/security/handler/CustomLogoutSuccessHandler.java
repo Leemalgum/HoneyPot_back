8b9aeb858beee4596ea3886908c580222eac7890
JiHyun Lee <supernova63@naver.com>
2024-05-16 09:35:05 +0900

    feat : security handler 들 추가

---
+++ beespring/src/main/java/com/beeSpring/beespring/security/handler/CustomLogoutSuccessHandler.java
@@ -0,0 +1,30 @@
+package com.beeSpring.beespring.security.handler;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import com.beeSpring.beespring.service.jwt.TokenService;
+
+@Component
+public class CustomLogoutSuccessHandler implements LogoutSuccessHandler {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Override
+    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        if (authentication != null) {
+            String userId = authentication.getName();
+            tokenService.deleteToken(userId);
+        }
+
+        response.sendRedirect("/login?logout");
+    }
+}
