815ab803e8a0589bde10867eafc3e06402bda06f
Leemalgum <withxia1215@naver.com>
2024-05-17 15:42:31 +0900

    refactor: productrepository findserialnumber jpql 수정

--- beespring/src/main/java/com/beeSpring/beespring/repository/bid/ProductRepository.java
+++ beespring/src/main/java/com/beeSpring/beespring/repository/bid/ProductRepository.java
@@ -1,10 +1,6 @@
 package com.beeSpring.beespring.repository.bid;
 
 import com.beeSpring.beespring.domain.bid.Product;
-import com.beeSpring.beespring.dto.mypage.ProductWithSerialNumberDTO;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -12,11 +8,12 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface ProductRepository  extends JpaRepository<Product, String> {
+public interface ProductRepository extends JpaRepository<Product, String> {
 
     @Query("select p, i.idolName from Product p join Idol i on p.idolId = i.idolId")
     List<Object[]> findAllWithIdolName();
-    
-    @Query("SELECT br.productId, br.paymentStatus, sh.deliveryStatus, p.serialNumber, p.productName, p.image1, p.priceUnit, p.startPrice, p.bidCnt FROM BidResult br JOIN Shipping sh ON br.bidResultId = sh.bidResult.bidResultId JOIN Product p ON br.productId = p.productId")
+
+    //    @Query("SELECT br.productId, br.paymentStatus, br.completeDate, sh.deliveryStatus, p.serialNumber, p.productName, p.image1, p.priceUnit, p.startPrice, p.bidCnt FROM BidResult br JOIN Shipping sh ON br.bidResultId = sh.bidResult.bidResultId JOIN Product p ON br.productId = p.productId")
+    @Query("SELECT br.productId, br.paymentStatus, br.completeDate, sh.deliveryStatus, p.serialNumber, p.productName, p.image1, p.priceUnit, p.startPrice, p.bidCnt, u.nickname FROM BidResult br JOIN Shipping sh ON br.bidResultId = sh.bidResult.bidResultId JOIN Product p ON br.productId = p.productId JOIN User u ON p.serialNumber = u.serialNumber")
     List<Object[]> findBySerialNumber(String serialNumber);
 }
