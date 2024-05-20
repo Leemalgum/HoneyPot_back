97094ecbfc9bc27ffa15beb7bfb16e4032b082a2
sowon901 <87898185+sowon901@users.noreply.github.com>
2024-05-20 12:26:02 +0900

    fix: 관심 아이돌 카테고리 관련 코드 수정 및 bidController, bidService, bidServiceImpl 코드 수정 (#20)
    * feature : 메인페이지 상품 불러오기
    
    * fix: 관심 아이돌 리스트 및 아이돌 상품 관련 코드 추가
    
    * fix: 경매 관련 코드 수정

--- beespring/src/main/java/com/beeSpring/beespring/controller/main/MainController.java
+++ beespring/src/main/java/com/beeSpring/beespring/controller/main/MainController.java
@@ -1,11 +1,15 @@
 package com.beeSpring.beespring.controller.main;
 
+import com.beeSpring.beespring.dto.category.IdolDTO;
 import com.beeSpring.beespring.dto.main.MainProductDTO;
 import com.beeSpring.beespring.service.main.MainService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,10 +22,29 @@ public class MainController {
     private final MainService mainService;
 
 
-    @GetMapping(path= "/filterByCategory")
-    public List<MainProductDTO> showProductsFilterByCategory(){
-        return mainService.filterByCategory();
+    @GetMapping(path= "/filterCategoryIdolName")
+    public List<IdolDTO> showIdolListByCategory(@PathVariable("serialNumber") String serialNumber){
+
+        
+
+        return mainService.showIdolCategoryName("123456789");
+
     }
+
+
+//    @GetMapping(path = "/filterByCategory")
+//    public ResponseEntity<List<MainProductDTO>> showProductsFilterByCategory(String idolName) {
+//        // productId를 이용하여 ProductService를 통해 상품 데이터를 가져옵니다.
+//        List<MainProductDTO> productDTO = mainService.filterByCategory(idolName);
+//        if (productDTO != null) {
+//            // 상품 데이터가 존재할 경우 200 OK 응답과 함께 데이터를 반환합니다.
+//            return new ResponseEntity<>(productDTO, HttpStatus.OK);
+//        } else {
+//            // 상품 데이터가 존재하지 않을 경우 404 Not Found 응답을 반환합니다.
+//            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+//        }
+//    }
+
 
     @GetMapping(path = "/filterByView")
     public List<MainProductDTO> showProductsFilterByView(){
