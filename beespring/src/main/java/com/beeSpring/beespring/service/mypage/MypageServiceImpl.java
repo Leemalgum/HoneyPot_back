3bf1bd188f54fa3d8f5a18b3697156afd12d706f
Leemalgum <withxia1215@naver.com>
2024-05-21 16:55:00 +0900

    refactor: mypageserviceimpl builder 수정

--- beespring/src/main/java/com/beeSpring/beespring/service/mypage/MypageServiceImpl.java
+++ beespring/src/main/java/com/beeSpring/beespring/service/mypage/MypageServiceImpl.java
@@ -23,6 +23,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -53,13 +54,15 @@ public class MypageServiceImpl implements MypageService {
             ProductWithSerialNumberDTO productDTO = ProductWithSerialNumberDTO.builder()
                     .productId((String) objArray[0])
                     .paymentStatus((int) objArray[1])
-                    .deliveryStatus((DeliveryStatus) objArray[2])
-                    .serialNumber((String) objArray[3])
-                    .productName((String) objArray[4])
-                    .image1((String) objArray[5])
-                    .priceUnit((Integer) objArray[6])
-                    .startPrice((int) objArray[7])
-                    .bidCnt((int) objArray[8])
+                    .completeDate((LocalDateTime) objArray[2])
+                    .deliveryStatus((DeliveryStatus) objArray[3])
+                    .serialNumber((String) objArray[4])
+                    .productName((String) objArray[5])
+                    .image1((String) objArray[6])
+                    .priceUnit((Integer) objArray[7])
+                    .startPrice((int) objArray[8])
+                    .bidCnt((int) objArray[9])
+                    .nickName((String) objArray[10])
                     .build();
 
             products.add(productDTO);
@@ -99,29 +102,39 @@ public class MypageServiceImpl implements MypageService {
     }
 
     public PaymentProductDTO getProductById(String serialNumber, String productId) {
-//        Object product = productRepository.findByProductId(serialNumber, productId);
-//        Map<String, Object> productMap = (Map<String, Object>) product;
-//
-//        PaymentProductDTO productDTO = PaymentProductDTO.builder()
-//                .productId((String) productMap.get("productId"))
-//                .serialNumber((String) productMap.get("serialNumber"))
-//                .productName((String) productMap.get("productName"))
-//                .priceUnit((int) productMap.get("priceUnit"))
-//                .startPrice((int) productMap.get("startPrice"))
-//                .bidCnt((Integer) productMap.get("bidCnt"))
-//                .recipientName((String) productMap.get("recipientName"))
-//                .postCode((String) productMap.get("postCode"))
-//                .roadAddress((String) productMap.get("roadAddress"))
-//                .detailAddress((String) productMap.get("detailAddress"))
-//                .recipientPhone((String) productMap.get("recipientPhone"))
-//                .build();
+        List<Object[]> productDataList = productRepository.findByProductId(serialNumber, productId);
+        Object[] productData = productDataList.get(0);
 
-        return null;
+        if (productData != null) {
+            try {
+                PaymentProductDTO productDTO = PaymentProductDTO.builder()
+                        .productId(productId)
+                        .serialNumber(serialNumber)
+                        .productName((String) productData[2])
+                        .priceUnit((Integer) productData[3])
+                        .startPrice((Integer) productData[4])
+                        .bidCnt((Integer) productData[5])
+                        .recipientName((String) productData[6])
+                        .postCode((String) productData[7])
+                        .roadAddress((String) productData[8])
+                        .detailAddress((String) productData[9])
+                        .recipientPhone((String) productData[10])
+                        .build();
+                logger.info("service: {}", productDTO);
+                return productDTO;
+            } catch (Exception e) {
+                logger.error("Error building productDTO", e);
+                return null;
+            }
+        } else {
+            logger.info("null");
+            return null;
+        }
     }
 
     //프로필 파트
-        @Override
-        public UserDTO getProfile(String serialNumber) {
+    @Override
+    public UserDTO getProfile(String serialNumber) {
         User user = userRepository.findBySerialNumber(serialNumber)
                 .orElseThrow(() -> new RuntimeException("User not found"));
         return new UserDTO(
@@ -148,6 +161,7 @@ public class MypageServiceImpl implements MypageService {
                 user.getTag3()
         );
     }
+
     @Override
     @Transactional
     public void saveProfile(String serialNumber, UserDTO userDTO, MultipartFile profileImageFile) throws IOException {
