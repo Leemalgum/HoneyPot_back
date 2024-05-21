7d0c8bb09c034bf566a1e40e64970aa782a37c1d
Leemalgum <withxia1215@naver.com>
2024-05-21 16:53:59 +0900

    refactor: productrepository findserialnumber, findproductid jpql 수정

--- beespring/src/main/java/com/beeSpring/beespring/repository/bid/ProductRepository.java
+++ beespring/src/main/java/com/beeSpring/beespring/repository/bid/ProductRepository.java
@@ -11,10 +11,16 @@ import java.util.List;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, String> {
 
-    @Query("select p, i.idolName from Product p join Idol i")
+    @Query("SELECT p, i.idolName FROM Product p JOIN p.idol i")
     List<Object[]> findAllWithIdolName();
 
-    @Query("SELECT p.productId, br.paymentStatus, br.completeDate, sh.deliveryStatus, u.serialNumber, p.productName, p.image1, p.priceUnit, p.startPrice, p.bidCnt, u.nickname FROM Shipping sh JOIN sh.bidResult br JOIN br.product p JOIN p.user u WHERE u.serialNumber = :serialNumber\n")
+    //    @Query("SELECT p.productId, br.paymentStatus, br.completeDate, sh.deliveryStatus, u.serialNumber, p.productName, p.image1, p.priceUnit, p.startPrice, p.bidCnt, u.nickname FROM Shipping sh JOIN sh.bidResult br JOIN br.product p JOIN p.user u WHERE u.serialNumber = :serialNumber\n")
+    @Query("SELECT p.productId, br.paymentStatus, br.completeDate, sh.deliveryStatus, u.serialNumber, p.productName, p.image1, p.priceUnit, p.startPrice, p.bidCnt, u.nickname " +
+            "FROM Shipping sh " +
+            "JOIN sh.bidResult br " +
+            "JOIN br.product p " +
+            "JOIN p.user u " +
+            "WHERE u.serialNumber = :serialNumber")
     List<Object[]> findBySerialNumber(String serialNumber);
 
 //    @Query("SELECT Product.productId, Product.serialNumber, Product.productName, Product.priceUnit, Product.startPrice, Product.bidCnt, Shipping_address.addressName, Shipping_address.recipientName, Shipping_address.postCode, Shipping_address.roadAddress, Shipping_address.detailAddress, Shipping_address.recipientPhone FROM Product JOIN User ON Product.serialNumber = User.serialNumber JOIN Shipping_address ON Product.serialNumber = Shipping_address.serialNumber WHERE Product.serialNumber = '123456789' AND Product.productId = 'A02'")
@@ -24,6 +30,17 @@ public interface ProductRepository extends JpaRepository<Product, String> {
 
 //    @Query("SELECT p.productId, p.serialNumber, p.productName, p.priceUnit, p.startPrice, p.bidCnt, sa.recipientName, sa.postCode, sa.roadAddress, sa.detailAddress, sa.recipientPhone FROM Product p JOIN p.user u JOIN p.shippingAddress sa WHERE p.serialNumber = :serialNumber AND p.productId = :productId")
 //    Object findByProductId(@Param("serialNumber") String serialNumber, @Param("productId") String productId);
+
+//    @Query("SELECT p.productId, p.serialNumber, p.productName, p.priceUnit, p.startPrice, p.bidCnt, " +
+//            "sa.recipientName, sa.postCode, sa.roadAddress, sa.detailAddress, sa.recipientPhone " +
+//            "FROM Product p JOIN p.user u JOIN u.shippingAddresses sa " +
+//            "WHERE p.serialNumber = :serialNumber AND p.productId = :productId")
+
+    @Query("SELECT p.productId, u.serialNumber, p.productName, p.priceUnit, p.startPrice, p.bidCnt, " +
+            "sa.recipientName, sa.postCode, sa.roadAddress, sa.detailAddress, sa.recipientPhone " +
+            "FROM Product p JOIN p.user u JOIN u.shippingAddresses sa " +
+            "WHERE u.serialNumber = :serialNumber AND p.productId = :productId")
+    List<Object[]> findByProductId(@Param("serialNumber") String serialNumber, @Param("productId") String productId);
 
 
 }
