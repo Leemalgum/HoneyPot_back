71b9e5d870b49675955a2991722b8fa867be55f3
JiHyun Lee <supernova63@naver.com>
2024-05-22 14:45:28 +0900

    refactor : OAuthRepository 수정
    provider 와 providerId 로 검색하던 것에서 provider와 userId로 검색하는 것으로 수정

--- beespring/src/main/java/com/beeSpring/beespring/repository/user/OAuthUserRepository.java
+++ beespring/src/main/java/com/beeSpring/beespring/repository/user/OAuthUserRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface OAuthUserRepository extends JpaRepository<OAuthUser, Long> {
-    Optional<OAuthUser> findByProviderAndProviderId(String provider, String providerId);
+    Optional<OAuthUser> findByProviderAndUserId(String provider, String UserId);
 }
 
