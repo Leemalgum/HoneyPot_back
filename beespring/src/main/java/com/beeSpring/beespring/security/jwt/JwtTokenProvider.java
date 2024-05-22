e4b02e1ccc3854f33726146384d6d4f9cfc7171f
JiHyun Lee <supernova63@naver.com>
2024-05-22 14:51:17 +0900

    refactor : JwtTokenProvider 수정
    1) access 토큰 만료 설정만 있었으나 refresh 토큰 만료 설정도 추가
    2) createRefreshToken, getExpirationDate, resolveToken 메서드 추가

--- beespring/src/main/java/com/beeSpring/beespring/security/jwt/JwtTokenProvider.java
+++ beespring/src/main/java/com/beeSpring/beespring/security/jwt/JwtTokenProvider.java
@@ -5,20 +5,34 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 import java.security.Key;
 import java.util.Date;
 
+@RequiredArgsConstructor
 @Component
 public class JwtTokenProvider {
 
     @Value("${spring.jwt.secret}")
     private String secretKey;
 
-    @Value("${spring.jwt.expiration}")
+    @Value("${spring.jwt.expiration.access}")
     private long validityInMilliseconds;
+
+    @Value("${spring.jwt.expiration.refresh}")
+    private long refreshValidityInMilliseconds;
+
+    private final UserDetailsService userDetailsService;
 
     private Key key;
 
@@ -27,8 +41,8 @@ public class JwtTokenProvider {
         this.key = Keys.hmacShaKeyFor(secretKey.getBytes());
     }
 
-    public String createToken(String username) {
-        Claims claims = (Claims)Jwts.claims().setSubject(username);
+    public String createAccessToken(String username) {
+        Claims claims = Jwts.claims().setSubject(username).build();
         Date now = new Date();
         Date validity = new Date(now.getTime() + validityInMilliseconds);
 
@@ -40,6 +54,26 @@ public class JwtTokenProvider {
                 .compact();
     }
 
+    public String createRefreshToken(String username) {
+        Claims claims = Jwts.claims().setSubject(username).build();
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + refreshValidityInMilliseconds); // Refresh Token 유효기간
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(validity)
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+
+    // 인증 정보 조회
+    public Authentication getAuthentication(String token) {
+        UserDetails userDetails = userDetailsService.loadUserByUsername(this.getUsername(token));
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+
     public String getUsername(String token) {
         return Jwts.parser()
                 .setSigningKey(key)
@@ -47,6 +81,17 @@ public class JwtTokenProvider {
                 .parseClaimsJws(token)
                 .getBody()
                 .getSubject();
+    }
+
+    // 토큰에서 만료 날짜를 추출하여 LocalDateTime으로 반환
+    public LocalDateTime getExpirationDate(String token) {
+        Date expiration = Jwts.parser()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getExpiration();
+        return LocalDateTime.ofInstant(expiration.toInstant(), ZoneId.systemDefault());
     }
 
     public boolean validateToken(String token) {
@@ -59,5 +104,14 @@ public class JwtTokenProvider {
         } catch (Exception e) {
             return false;
         }
+    }
+
+    // Request의 Header에서 token 값 가져오기
+    public String resolveToken(HttpServletRequest req) {
+        String bearerToken = req.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
     }
 }
