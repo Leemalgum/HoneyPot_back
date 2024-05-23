031db2c1a85dc6ee4b1c626d4d9a61c2a1dfc72f
JiHyun Lee <supernova63@naver.com>
2024-05-23 11:48:00 +0900

    comment : CorsConfig 주석 수정

--- beespring/src/main/java/com/beeSpring/beespring/security/config/CorsConfig.java
+++ beespring/src/main/java/com/beeSpring/beespring/security/config/CorsConfig.java
@@ -9,7 +9,6 @@ import org.springframework.web.filter.CorsFilter;
 
 /**
  * CorsConfig 를 통해 스프링 부트 애플리케이션이 모든 도메인에서의 요청을 허용하고, 특정 헤더를 노출하며, 다양한 HTTP 메서드를 지원할 수 있게된다.
- * 이 설명이 애초에 포트 번호 다른 서버 (프론트와 백처럼) 간의 통신 때문에 그런건데 아래처럼 beespring.shop 으로 제한할 수 있는지 확인해 보아야 한다.
  * */
 @Configuration
 public class CorsConfig {
@@ -21,7 +20,6 @@ public class CorsConfig {
         config.setAllowCredentials(true); // 자격 증명 (쿠키, 인증 헤더 등) 을 허용하도록 설정
         config.addAllowedOrigin("http://localhost:3000"); // 모든 도메인에서의 요청을 허용 (와일드카드 문법을 허용하는 메서드)
         config.addAllowedOrigin("http://localhost:8080"); // 모든 도메인에서의 요청을 허용 (와일드카드 문법을 허용하는 메서드)
-        // 꼭 !!!!! 배포 후에는 config.addAllowedOriginPatter("http://beespring.shop")로 바꿔야 한다. 안그러면 위에 true 설정 때문에 보안 엄청 취약해짐
         config.addAllowedHeader("*"); // 모든 헤더를 허용
         config.addAllowedMethod("*"); // 모든 HTTP 메서드 (GET, POST, PUT, DELETE 등)을 허용
         config.addExposedHeader("Authorization"); // 클라이언트가 'Authorization' 헤더에 접근할 수 있게 한다
