661389a48a40467c6096bebd5a146860e2f6c64f
Leemalgum <withxia1215@naver.com>
2024-05-23 12:07:38 +0900

    refactor: productrepository findbyserialnumber jpql 수정

--- beespring/src/main/java/com/beeSpring/beespring/repository/bid/ProductRepository.java
+++ beespring/src/main/java/com/beeSpring/beespring/repository/bid/ProductRepository.java
@@ -15,26 +15,19 @@ public interface ProductRepository extends JpaRepository<Product, String> {
     List<Object[]> findAllWithIdolName();
 
     //    @Query("SELECT p.productId, br.paymentStatus, br.completeDate, sh.deliveryStatus, u.serialNumber, p.productName, p.image1, p.priceUnit, p.startPrice, p.bidCnt, u.nickname FROM Shipping sh JOIN sh.bidResult br JOIN br.product p JOIN p.user u WHERE u.serialNumber = :serialNumber\n")
-    @Query("SELECT p.productId, br.paymentStatus, br.completeDate, sh.deliveryStatus, u.serialNumber, p.productName, p.image1, p.priceUnit, p.startPrice, p.bidCnt, u.nickname " +
+//    @Query("SELECT p.productId, br.paymentStatus, br.completeDate, sh.deliveryStatus, u.serialNumber, p.productName, p.image1, p.priceUnit, p.startPrice, p.bidCnt, u.nickname " +
+//            "FROM Shipping sh " +
+//            "JOIN sh.bidResult br " +
+//            "JOIN br.product p " +
+//            "JOIN p.user u " +
+//            "WHERE u.serialNumber = :serialNumber")
+    @Query("SELECT p.productId, br.paymentStatus, br.completeDate, sh.deliveryStatus, p.user.serialNumber, p.productName, p.image1, p.priceUnit, p.startPrice, p.bidCnt, u.nickname, p.storageStatus " +
             "FROM Shipping sh " +
             "JOIN sh.bidResult br " +
-            "JOIN br.product p " +
+            "RIGHT JOIN br.product p " +
             "JOIN p.user u " +
-            "WHERE u.serialNumber = :serialNumber")
+            "WHERE p.user.serialNumber = :serialNumber")
     List<Object[]> findBySerialNumber(String serialNumber);
-
-//    @Query("SELECT Product.productId, Product.serialNumber, Product.productName, Product.priceUnit, Product.startPrice, Product.bidCnt, Shipping_address.addressName, Shipping_address.recipientName, Shipping_address.postCode, Shipping_address.roadAddress, Shipping_address.detailAddress, Shipping_address.recipientPhone FROM Product JOIN User ON Product.serialNumber = User.serialNumber JOIN Shipping_address ON Product.serialNumber = Shipping_address.serialNumber WHERE Product.serialNumber = '123456789' AND Product.productId = 'A02'")
-//    Object findByProductId(String serialNumber, String productId);
-//    @Query("SELECT p.productId, p.serialNumber, p.productName, p.priceUnit, p.startPrice, p.bidCnt, sa.recipientName, sa.postCode, sa.roadAddress, sa.detailAddress, sa.recipientPhone FROM Product p JOIN p.user u JOIN p.shippingAddress sa WHERE p.serialNumber = :serialNumber AND p.productId = :productId")
-//    Object findByProductId(@Param("serialNumber") String serialNumber, @Param("productId") String productId);
-
-//    @Query("SELECT p.productId, p.serialNumber, p.productName, p.priceUnit, p.startPrice, p.bidCnt, sa.recipientName, sa.postCode, sa.roadAddress, sa.detailAddress, sa.recipientPhone FROM Product p JOIN p.user u JOIN p.shippingAddress sa WHERE p.serialNumber = :serialNumber AND p.productId = :productId")
-//    Object findByProductId(@Param("serialNumber") String serialNumber, @Param("productId") String productId);
-
-//    @Query("SELECT p.productId, p.serialNumber, p.productName, p.priceUnit, p.startPrice, p.bidCnt, " +
-//            "sa.recipientName, sa.postCode, sa.roadAddress, sa.detailAddress, sa.recipientPhone " +
-//            "FROM Product p JOIN p.user u JOIN u.shippingAddresses sa " +
-//            "WHERE p.serialNumber = :serialNumber AND p.productId = :productId")
 
     @Query("SELECT p.productId, u.serialNumber, p.productName, p.priceUnit, p.startPrice, p.bidCnt, " +
             "sa.addressId, sa.addressName, sa.recipientName, sa.recipientPhone, sa.postCode, sa.roadAddress, sa.detailAddress " +
