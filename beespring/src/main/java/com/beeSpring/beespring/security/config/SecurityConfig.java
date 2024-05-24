5318544d1baa1f95c05ed6614067fdb6dd9d70e4
JiHyun Lee <supernova63@naver.com>
2024-05-24 17:20:07 +0900

    refactor : requestMatchers에 "/api/**" 추가

--- beespring/src/main/java/com/beeSpring/beespring/security/config/SecurityConfig.java
+++ beespring/src/main/java/com/beeSpring/beespring/security/config/SecurityConfig.java
@@ -12,7 +12,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -55,7 +54,7 @@ public class SecurityConfig {
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .authorizeRequests(auth -> auth
-                        .requestMatchers("/index","/", "/auth/**", "/login", "/signup", "/api/oauth2/**").permitAll()
+                        .requestMatchers("/index","/", "/auth/**", "/login", "/signup", "/api/oauth2/**", "/api/**").permitAll()
                         .requestMatchers("/api/v1/user/**").hasAnyRole("USER", "ADMIN")
                         .requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
