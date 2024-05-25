5876177d422ac2e3aca72fcbbc212eb61dd374fe
JiHyun Lee <supernova63@naver.com>
2024-05-25 17:11:10 +0900

    refactor : auto_increment 를 위한 어노테이션 추가
    @GeneratedValue(strategy = GenerationType.IDENTITY)

--- beespring/src/main/java/com/beeSpring/beespring/domain/user/UserIdol.java
+++ beespring/src/main/java/com/beeSpring/beespring/domain/user/UserIdol.java
@@ -2,15 +2,19 @@ package com.beeSpring.beespring.domain.user;
 
 import com.beeSpring.beespring.domain.category.Idol;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name="User_Idol")
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class UserIdol {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="user_idol_id")
     private int userIdolId;
 
