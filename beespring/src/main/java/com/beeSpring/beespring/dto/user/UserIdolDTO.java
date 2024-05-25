a7d10ab874abab6d9fba4c986ebfb0e6a8dec8fc
JiHyun Lee <supernova63@naver.com>
2024-05-25 23:45:23 +0900

    feat : UserIdolDTO 추가

--- beespring/src/main/java/com/beeSpring/beespring/dto/user/UserIdolDTO.java
+++ beespring/src/main/java/com/beeSpring/beespring/dto/user/UserIdolDTO.java
@@ -1,4 +1,14 @@
 package com.beeSpring.beespring.dto.user;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserIdolDTO {
+    private int userIdolId;
+    private String serialNumber;
+    private int idolId;
 }
