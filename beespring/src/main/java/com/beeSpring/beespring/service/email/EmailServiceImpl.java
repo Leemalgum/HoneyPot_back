8b12c94cc053ee6d5bb0c106466887b39f4c9817
JiHyun Lee <supernova63@naver.com>
2024-05-28 14:13:25 +0900

    refactor : sendPasswordResetEmail 의 리턴 타입 수정, 로고 이미지 삭제

--- beespring/src/main/java/com/beeSpring/beespring/service/email/EmailServiceImpl.java
+++ beespring/src/main/java/com/beeSpring/beespring/service/email/EmailServiceImpl.java
@@ -83,9 +83,9 @@ public class EmailServiceImpl implements EmailService {
     }
 
     @Override
-    public void sendPasswordResetEmail(String to, String resetUrl) {
+    public MimeMessage sendPasswordResetEmail(String to, String resetUrl) {
         MimeMessage message = javaMailSender.createMimeMessage();
-
+        System.out.println("이메일 주소 : " + to);
         try {
             MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
             helper.setFrom(senderEmail);
@@ -95,18 +95,15 @@ public class EmailServiceImpl implements EmailService {
             String body = "<h1>비밀번호 재설정 요청</h1>";
             body += "<p>비밀번호를 재설정하시려면 아래 링크를 클릭해주세요:</p>";
             body += "<a href='" + resetUrl + "'>비밀번호 재설정</a>";
-            body += "<p>감사합니다.<br>꿀단지 팀</p>";
+            body += "<p>감사합니다.<br><br>꿀단지 팀</p>";
 
             helper.setText(body, true); // true를 설정하여 HTML을 사용하도록 함
 
-            // 로고 이미지 첨부 (옵션)
-            // ClassPathResource resource = new ClassPathResource("static/logo.png"); // 로고 이미지의 경로를 설정
-            // helper.addInline("logoImage", resource);
         } catch (Exception e) {
             e.printStackTrace();
         }
-
         javaMailSender.send(message);
+        return message;
     }
 
 }
