d1b2e4c1797f3b006fbd947bcc6d630bf4f8f013
TaeJinKim0930 <tjmax0930@gmail.com>
2024-05-29 10:34:18 +0900

    Fix: 전체적인 결제 시스템 기본틀 최종 버전 업데이트 완료
    Fix: 전체적인 결제 시스템 기본틀 최종 버전 업데이트 완료

--- beespring/src/main/java/com/beeSpring/beespring/dto/payment/PaymentDTO.java
+++ beespring/src/main/java/com/beeSpring/beespring/dto/payment/PaymentDTO.java
@@ -1,34 +1,30 @@
 package com.beeSpring.beespring.dto.payment;
 
-import lombok.Data;
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.time.LocalDate;
 
 @Data
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
 public class PaymentDTO {
-    private String order_id;
+
+    private String merchantUid; // order_id
+    private String serialNumber;
     private String pg;
     private String payMethod;
     private boolean escrow;
-    private String vbankDue; // 가상계좌 입금 기간 설정(필요x)
-    private String bizNum;
-    private String quota;
-    private String merchantUid;
     private String name;
     private int amount;
     private String buyerName;
-    private String buyerPhone;
-    private String buyerEmail;
-    private String buyerAddr;
-    private String buyerPostcode;
-
-    private String imp_uid;
-    private String paid_amount;
-    private String paid_at;
-    private String pay_method;
+    private LocalDate dateAdded;
     private String status;
-    private String receipt_url;
-    private String pg_provider;
-    private String pg_tid;
-    private String card_name;
-    private String card_number;
+    private boolean success;
+    private String receiptUrl;
+    private String productId;
 
 }
