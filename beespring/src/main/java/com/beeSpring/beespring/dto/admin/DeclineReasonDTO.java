dcb3beb04d02e3ea126f56ab193690423cf01c70
JiHyun Lee <supernova63@naver.com>
2024-05-30 07:20:05 +0900

    feat : 입고 거절 이유 DTO 추가

---
+++ beespring/src/main/java/com/beeSpring/beespring/dto/admin/DeclineReasonDTO.java
@@ -0,0 +1,10 @@
+package com.beeSpring.beespring.dto.admin;
+
+import lombok.Data;
+
+@Data
+public class DeclineReasonDTO {
+    private String productId;
+    private String serialNumber;
+    private String declineReason;
+}
