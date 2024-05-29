e20dde19dd5bf9cf96b282299c0ba37641b3074f
JiHyun Lee <supernova63@naver.com>
2024-05-30 07:15:27 +0900

    refactor : 회원의 상태 변경을 하는 메서드 updateUserState 추가

--- beespring/src/main/java/com/beeSpring/beespring/repository/user/UserRepository.java
+++ beespring/src/main/java/com/beeSpring/beespring/repository/user/UserRepository.java
@@ -1,32 +1,38 @@
 package com.beeSpring.beespring.repository.user;
 
 import com.beeSpring.beespring.domain.user.OAuthUser;
+import com.beeSpring.beespring.domain.user.State;
 import com.beeSpring.beespring.domain.user.User;
+import com.beeSpring.beespring.dto.user.ManageUserDTO;
+import com.beeSpring.beespring.dto.user.ManageUserProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
     Boolean existsByUserId(String userId);
+
     /**
      * 아이디를 직접 입력하는 서비스 자체 로그인
      * sql injection 을 예방하기 위해 userId를 parameter 로 binding 함
-     * */
+     */
     @Query("SELECT u FROM User u WHERE u.userId = :userId")
     User findByUserId(@Param("userId") String userId);
 
     /**
      * OAuth 서비스에서 provider, user id(key code)가 둘 다 필요할 때 활용
-     * */
+     */
     Optional<User> findByProviderAndUserId(String provider, String UserId);
 
     /**
      * serialNumber 중복 방지
-     * */
+     */
     boolean existsBySerialNumber(String serialNumber);
 
     Optional<User> findBySerialNumber(String serialNumber);
@@ -35,4 +41,11 @@ public interface UserRepository extends JpaRepository<User, String> {
 
     @Query("SELECT u FROM User u WHERE u.userId = :userId AND u.mobileNumber = :mobileNumber AND u.email = :email")
     Optional<User> findByUserIdAndMobileNumberAndEmail(@Param("userId") String userId, @Param("mobileNumber") String mobileNumber, @Param("email") String email);
+
+    @Query("SELECT u.serialNumber AS serialNumber, u.userId AS userId, u.gender AS gender, u.birthdate AS birthdate, u.registrationDate AS registrationDate, u.state AS state FROM User u")
+    List<ManageUserProjection> findAllUsersAsDTO();
+
+    @Modifying
+    @Query("UPDATE User u SET u.state = :state WHERE u.userId = :userId")
+    void updateUserState(@Param("userId") String userId, @Param("state") State state);
 }
