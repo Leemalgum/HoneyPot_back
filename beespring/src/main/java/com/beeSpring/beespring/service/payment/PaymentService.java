0bc2ba569e493921c46518bfb1f290b270265235
TaeJinKim0930 <108087591+TaeJinKim0930@users.noreply.github.com>
2024-05-30 09:22:21 +0900

    Feat: 결제 전체 조회를 위한 코드 및 테스트 코드작성 입니다 (#45)
    Feat: 결제 전체 조회를 위한 코드작성 입니다

--- beespring/src/main/java/com/beeSpring/beespring/service/payment/PaymentService.java
+++ beespring/src/main/java/com/beeSpring/beespring/service/payment/PaymentService.java
@@ -4,9 +4,11 @@ import com.beeSpring.beespring.domain.payment.Payment;
 import com.beeSpring.beespring.dto.mypage.ProductWithSerialNumberDTO;
 import com.beeSpring.beespring.dto.payment.PaymentDTO;
 
+import java.util.List;
+
 public interface PaymentService {
     public void processPayment(PaymentDTO paymentDTO);
-
+    public List<PaymentDTO> findAllPayment();
     public ProductWithSerialNumberDTO getPaymentStatus(String productId);
 
 }
